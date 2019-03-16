54df4fa33eb020e152a59f315fd1184223a68ffb
lnked <ed.proff@gmail.com>
2019-03-16 04:19:32 +0300

    upgrade dependencies

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,9 +1,11 @@
 import RNLanguages from 'react-native-languages'
 import i18n from 'i18n-js'
+
 import en from './locales/en'
+import ru from './locales/ru'
 
 i18n.locale = RNLanguages.language
 i18n.fallbacks = true
-i18n.translations = { en }
+i18n.translations = { en, ru }
 
 export default i18n
---
+++ src/i18n/locales/ru.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+export default {
+  global: {
+    hide: 'Скрыть',
+    show: 'Показать',
+  },
+
+  main: {
+    button: {
+      text: 'Тестовая кнопка',
+    },
+  },
+}
