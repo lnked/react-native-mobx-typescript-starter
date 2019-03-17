22b4593f3c42d41a6822924ec6d97433626cc38e
lnked <ed.proff@gmail.com>
2019-03-17 22:23:35 +0300

    upgrade

--- babel.config.js
+++ babel.config.js
@@ -6,10 +6,9 @@ module.exports = {
   plugins: [
     // // ['@babel/plugin-syntax-typescript'],
     // ['@babel/plugin-transform-flow-strip-types'],
-    // ['@babel/plugin-proposal-decorators', { 'legacy': true }],
-    // ['@babel/plugin-proposal-class-properties'],
-    // // ['@babel/plugin-proposal-class-properties', { 'loose': true }],
-    // ['@babel/plugin-proposal-object-rest-spread'],
+    ['@babel/plugin-proposal-decorators', { 'legacy': true }],
+    ['@babel/plugin-proposal-class-properties', { 'loose': true }],
+    ['@babel/plugin-proposal-object-rest-spread'],
     // // ['@babel/plugin-transform-runtime'],
     ['module-resolver', {
       'root': ['./src'],
---
+++ src/routes/stacks/home.tsx
@@ -0,0 +1,14 @@
+// const HomeStack = createStackNavigator({
+//   Home: HomeScreen,
+//   Details: DetailsScreen,
+// })
+
+// const SettingsStack = createStackNavigator({
+//   Settings: SettingsScreen,
+//   Profile: ProfileScreen,
+// })
+
+// const TabNavigator = createBottomTabNavigator({
+//   Home: HomeStack,
+//   Settings: SettingsStack,
+// })
