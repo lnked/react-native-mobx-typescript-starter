777c45aa128a12bae4cfdc887dcba2a3bc2e9f20
lnked <ed.proff@gmail.com>
2019-04-14 21:51:57 +0300

    install react-native-svg

--- android/app/src/main/java/com/app/MainApplication.java
+++ android/app/src/main/java/com/app/MainApplication.java
@@ -3,6 +3,7 @@ package com.app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.yonahforst.rnpermissions.RNPermissionsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -33,6 +34,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+        new SvgPackage(),
         new SplashScreenReactPackage(),
         new RNPermissionsPackage(),
         new RNGestureHandlerPackage(),
