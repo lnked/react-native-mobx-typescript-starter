e938169e29123deaafc178d326332bba0d9444a6
lnked <ed.proff@gmail.com>
2019-05-18 01:11:14 +0300

    upgrade devendencies

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -23,3 +23,16 @@ export const deviceHeight = height
 //     }),
 //   },
 // });
+
+const permissionTypes = {
+  location: PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+  camera: PermissionsAndroid.PERMISSIONS.CAMERA,
+  microphone: PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+  contacts: PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+  event: PermissionsAndroid.PERMISSIONS.READ_CALENDAR,
+  storage: PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE, // - here
+  photo: PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE, // - here
+  callPhone: PermissionsAndroid.PERMISSIONS.CALL_PHONE,
+  readSms: PermissionsAndroid.PERMISSIONS.READ_SMS,
+  receiveSms: PermissionsAndroid.PERMISSIONS.RECEIVE_SMS,
+}
