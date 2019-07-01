7b89dbf99fe8693c0df019c8ce3dc1c0f2dfb831
Edik Bulikyan <ed.proff@gmail.com>
2019-07-02 00:31:23 +0300

    upgrade dependences

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ import { AppRegistry, YellowBox } from 'react-native'
 
 import * as Reactotron from './reactotron-config'
 import { name as appName } from './app.json'
-import App from '@/app'
+import AppContainer from '@/app'
+
+// YellowBox.ignoreWarnings([
+//   'Module RCTImageLoader requires',
+//   'Warning: componentWillMount is deprecated',
+//   'Warning: componentWillReceiveProps is deprecated',
+//   'Warning: isMounted(...) is deprecated',
+// ])
 
 // console.disableYellowBox = true
 
-YellowBox.ignoreWarnings([
-  // 'Module RCTImageLoader requires',
-  // 'Warning: componentWillMount is deprecated',
-  // 'Warning: componentWillReceiveProps is deprecated',
-  // 'Warning: isMounted(...) is deprecated',
-])
-
 Reactotron.init(__DEV__)
-
-AppRegistry.registerComponent(appName, () => App)
+AppRegistry.registerComponent(appName, () => AppContainer)
