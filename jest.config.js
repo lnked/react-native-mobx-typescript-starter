381a122dd29fe31465bc3273ac3640878b82d239
Edik Bulikyan <ed.proff@gmail.com>
2019-07-13 03:59:24 +0300

    update

---
+++ jest.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  preset: 'react-native',
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  // transform: {
+  //   '\\.jsx?$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
+  //   '\\.tsx?$': 'ts-jest',
+  // },
+  cacheDirectory: '<rootDir>/.cache/.jest',
+  // moduleNameMapper: {
+  //   '^@/(.*)$': '<rootDir>/src/$1',
+  // },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+
+  // transformIgnorePatterns: [
+  //   'node_modules/(?!(jest-)?react-native|react-native-animatable|react-navigation|@react-navigation.*)',
+  // ],
+
+  // globals: {
+  //   'ts-jest': {
+  //     tsConfig: 'tsconfig.jest.json',
+  //     diagnostics: false,
+  //   },
+	// },
+
+  // Setup Enzyme
+  // snapshotSerializers: ['enzyme-to-json/serializer'],
+
+  // Setup Coverage
+  // coverageDirectory: '<rootDir>/coverage',
+  // coveragePathIgnorePatterns: ['/node_modules/', '/ios/', '/android/'],
+  // collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}', '!**/node_modules/**'],
+
+  verbose: false,
+};
