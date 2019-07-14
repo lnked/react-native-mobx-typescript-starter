cbb82d588824ebd861dbcbc802e82e99002c2598
Edik Bulikyan <ed.proff@gmail.com>
2019-07-14 05:20:19 +0300

    update

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,4 +1,5 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
+import * as RNLocalize from 'react-native-localize';
 import {
   SafeAreaView,
   ScrollView,
@@ -14,49 +15,74 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-import styles from './styles'
+import i18n, { setI18nConfig } from '@/i18n';
 
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
+import styles from './styles';
+
+class App extends Component {
+  constructor (props: any) {
+    super(props);
+
+    setI18nConfig();
+  }
+
+  componentDidMount() {
+    RNLocalize.addEventListener('change', this.handleLocalizationChange);
+  }
+
+  componentWillUnmount() {
+    RNLocalize.removeEventListener('change', this.handleLocalizationChange);
+  }
+
+  handleLocalizationChange = () => {
+    setI18nConfig();
+    this.forceUpdate();
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}>
+            <Header />
+            <View style={styles.body}>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>{i18n.t('test')}</Text>
+                <Text style={styles.sectionTitle}>{i18n.t('global.show')}</Text>
+                <Text style={styles.sectionTitle}>Step One</Text>
+                <Text style={styles.sectionDescription}>
+                  Edit <Text style={styles.highlight}>App.js</Text> to change this
+                  screen and then come back to see your edits.
+                </Text>
+              </View>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>See Your Changes</Text>
+                <Text style={styles.sectionDescription}>
+                  <ReloadInstructions />
+                </Text>
+              </View>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Debug</Text>
+                <Text style={styles.sectionDescription}>
+                  <DebugInstructions />
+                </Text>
+              </View>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Learn More</Text>
+                <Text style={styles.sectionDescription}>
+                  Read the docs to discover what to do next:
+                </Text>
+              </View>
+              <LearnMoreLinks />
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
+          </ScrollView>
+        </SafeAreaView>
+      </Fragment>
+    );
+  }
 };
 
 export default App;
--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,11 +1,37 @@
-import RNLocalize from 'react-native-localize'
-import i18n from 'i18n-js'
+import * as RNLocalize from 'react-native-localize';
+import { I18nManager } from 'react-native';
+import memoize from 'lodash.memoize';
+import i18n from 'i18n-js';
 
-import en from './locales/en'
-import ru from './locales/ru'
+const translationGetters: any = {
+  en: () => require('./translations/en.json'),
+  ru: () => require('./translations/ru.json'),
+};
 
-i18n.locale = RNLocalize.language
-i18n.fallbacks = true
-i18n.translations = { en, ru }
+const translate = memoize(
+  (key: string, config: any) => i18n.t(key, config),
+  (key: string, config: any) => (config ? key + JSON.stringify(config) : key),
+);
 
-export default i18n
+export const setI18nConfig = () => {
+  // fallback if no available language fits
+  const fallback = { languageTag: 'en', isRTL: false };
+
+  const { languageTag, isRTL } =
+    RNLocalize.findBestAvailableLanguage(Object.keys(translationGetters)) ||
+    fallback;
+
+  // clear translation cache
+  translate.cache && translate.cache.clear && translate.cache.clear();
+
+  // update layout direction
+  I18nManager.forceRTL(isRTL);
+
+  // set i18n-js config
+  i18n.translations = { [languageTag]: translationGetters[languageTag]() };
+  i18n.locale = languageTag; // RNLocalize.language;
+
+  i18n.fallbacks = true;
+};
+
+export default i18n;
