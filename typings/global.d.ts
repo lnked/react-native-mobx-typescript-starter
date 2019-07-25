0d7061b8ee5a9683e6e1a0ddb15f21af41dad6c0
Edik Bulikyan <ed.proff@gmail.com>
2019-07-25 03:11:40 +0300

    update

---
+++ typings/global.d.ts
@@ -0,0 +1,9 @@
+interface Window {
+  __INITIAL_STATE__: any;
+  __DEVTOOLS_EXTENSION__: () => any;
+}
+
+declare module '@/*'
+declare module 'emotion-theming'
+declare module '@emotion/core'
+declare module '@emotion/styled'
