a8eb7197a2b6460283eb5b542f797c08aff28951
Edik Bulikyan <ed.proff@gmail.com>
2019-07-26 18:00:00 +0300

    upgrade dependencies

--- src/services/middleware/Crashlytics.ts
+++ src/services/middleware/Crashlytics.ts
@@ -1,26 +1,26 @@
-var Fabric = require('react-native-fabric');
+// var Fabric = require('react-native-fabric');
 
-var { Crashlytics } = Fabric;
+// var { Crashlytics } = Fabric;
 
-Crashlytics.setUserName('megaman');
+// Crashlytics.setUserName('megaman');
 
-Crashlytics.setUserEmail('user@email.com');
+// Crashlytics.setUserEmail('user@email.com');
 
-Crashlytics.setUserIdentifier('1234');
+// Crashlytics.setUserIdentifier('1234');
 
-Crashlytics.setBool('has_posted', true);
+// Crashlytics.setBool('has_posted', true);
 
-Crashlytics.setString('organization', 'Acme. Corp');
+// Crashlytics.setString('organization', 'Acme. Corp');
 
-// Forces a native crash for testing
-Crashlytics.crash();
+// // Forces a native crash for testing
+// Crashlytics.crash();
 
-// Due to differences in primitive types between iOS and Android
-// I've exposed a setNumber function vs. setInt, setFloat, setDouble, setLong, etc
-Crashlytics.setNumber('post_count', 5);
+// // Due to differences in primitive types between iOS and Android
+// // I've exposed a setNumber function vs. setInt, setFloat, setDouble, setLong, etc
+// Crashlytics.setNumber('post_count', 5);
 
-// Record a non-fatal JS error only on Android
-Crashlytics.logException('');
+// // Record a non-fatal JS error only on Android
+// Crashlytics.logException('');
 
-// Record a non-fatal JS error only on iOS
-Crashlytics.recordError('something went wrong!');
+// // Record a non-fatal JS error only on iOS
+// Crashlytics.recordError('something went wrong!');
