4a5580b724547b42512c00e643a2cdb6d6a74aeb
Edik Bulikyan <ed.proff@gmail.com>
2019-07-26 17:59:48 +0300

    upgrade dependencies

---
+++ src/services/middleware/Answers.ts
@@ -0,0 +1,22 @@
+// var Fabric = require('react-native-fabric');
+
+// var { Answers } = Fabric;
+
+// // All log functions take an optional object of custom attributes as the last parameter
+// Answers.logCustom('Performed a custom event', { bigData: true });
+
+// Answers.logContentView('To-Do Edit', 'To-Do', 'to-do-42', { userId: 93 });
+
+// Answers.logAddToCart(24.50, 'USD', 'Air Jordans', 'shoes', '987654', {color: 'red'});
+
+// Answers.logInvite('Facebook');
+
+// Answers.logLogin('Twitter', true);
+
+// Answers.logSearch('React Native');
+
+// Answers.logShare('Twitter', 'Big news article', 'post', '1234');
+
+// Answers.logSignUp('Twitter', true);
+
+// Answers.logPurchase(24.99,'USD',true, 'Air Jordans', 'shoes', '987654');
---
+++ src/services/middleware/Crashlytics.ts
@@ -0,0 +1,26 @@
+var Fabric = require('react-native-fabric');
+
+var { Crashlytics } = Fabric;
+
+Crashlytics.setUserName('megaman');
+
+Crashlytics.setUserEmail('user@email.com');
+
+Crashlytics.setUserIdentifier('1234');
+
+Crashlytics.setBool('has_posted', true);
+
+Crashlytics.setString('organization', 'Acme. Corp');
+
+// Forces a native crash for testing
+Crashlytics.crash();
+
+// Due to differences in primitive types between iOS and Android
+// I've exposed a setNumber function vs. setInt, setFloat, setDouble, setLong, etc
+Crashlytics.setNumber('post_count', 5);
+
+// Record a non-fatal JS error only on Android
+Crashlytics.logException('');
+
+// Record a non-fatal JS error only on iOS
+Crashlytics.recordError('something went wrong!');
